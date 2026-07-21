62ff2381cc5e0424c7f43ff41a213109ca490e65
Mizar Nevelli <mizarnevelli@gmail.com>
2026-07-21 23:41:01 +0800

    fix:stats

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -152,7 +152,7 @@ export function HomePage() {
             <StatCard value="∞" label={t("home.stats.coffees")} />
           </ScrollReveal>
           <ScrollReveal delay={0.3}>
-            <StatCard value="1" label={t("home.stats.eventLoop")} />
+            <StatCard value="10" label={t("home.stats.cigarettes")} />
           </ScrollReveal>
         </div>
       </section>
