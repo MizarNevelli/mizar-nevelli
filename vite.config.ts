fc5c6a3d55c98132ec3623f84bbcac046e5d352a
Mizar Nevelli <mizarnevelli@gmail.com>
2026-08-11 15:27:19 +0800

    fix:config

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ import {
 import { join, extname } from "path";
 import sharp from "sharp";
 
-const SITE_URL = process.env.VITE_SITE_URL ?? "https://mizarnevelli.js.org";
+const SITE_URL = process.env.VITE_SITE_URL ?? "https://mizarnevelli.vercel.app";
 const DEFAULT_OG = `${SITE_URL}/og.png`;
 
 type RouteMeta = {
